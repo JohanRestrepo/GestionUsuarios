b9f003c3c5abb22efb206553e3a3af619cb8ef9c
Zarigueya <esnic1994@gmail.com>
2026-02-24 05:05:40 -0500

    puse decente las tarjetas de usuario

--- src/component/User.js
+++ src/component/User.js
@@ -1,13 +1,21 @@
 import React from "react";
+import '../style/User.css';
 
 function User(props){
     return (
-        <div>
-            <img src={props.picture} alt={props.name}></img>
-            <h1>{props.name}</h1>
-            <p>{props.email}</p>
-            <p>{props.cell}</p>
-            <p>ubicacion: {props.location} - {props.city}</p>
+        <div className="contendor-usuario">
+            <div>
+                <img className='imagen-usuario'
+                src={props.picture} 
+                alt={props.name}>
+                </img>
+            </div>
+            <div className='contenedor-texto'>
+                <p className='nombre'><strong>{props.name} {props.lastname}</strong></p>
+                <p>Email: {props.email}</p>
+                <p>Cel: {props.cell}</p>
+                <p className='ubicacion'>ubicacion: {props.location} - {props.city}</p>
+            </div>
         </div>
     )
 }
--- src/component/UserList.js
+++ src/component/UserList.js
@@ -8,27 +8,30 @@ function UserList(){
     const {currentList} = useContext(UserContext);
 
     return(
-        <div>
-            <h1>Usuarios en el sistema</h1>
-            
-            {
+        <>
+        <div className='contenedor-titulo'>
+            <h1 className='titulo'>Usuarios en el sistema</h1>
+        </div>
+            <div className='contenedor-usuarios'>
+                {
                     currentList.results.map((user) => {
                             return (
                                 <User 
                                     key={user.user.username}
                                     picture={user.user.picture.medium}
                                     name={user.user.name.first}
+                                    lastname={user.user.name.last}
                                     email={user.user.email}
                                     cell={user.user.cell}
                                     location={user.user.location.street}
                                     city={user.user.location.city}
-                                />
-
-                                
+                                />         
                     )
-                })
-            }
-        </div>     
+                    })
+                }
+            </div>
+            
+        </>     
     )
 }
 
